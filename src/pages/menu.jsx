7c79199fcc3d726f6b3714221d23da1aa4579217
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-01-31 16:42:16 +0100

    no more document

--- src/pages/menu.jsx
+++ src/pages/menu.jsx
@@ -39,33 +39,6 @@ const menuPage = (props) => {
     </div>
   );
 
-  // useEffect(() => {
-  const nav = document.getElementById("nav");
-
-  //   const scrollUp = "scroll-up";
-  //   const scrollDown = "scroll-down";
-  //   let lastScroll = 0;
-  //   window.addEventListener("scroll", () => {
-  //     const currentScroll = window.pageYOffset;
-  //     if (currentScroll <= 0) {
-  //       nav.classList.remove(scrollUp);
-  //       return;
-  //     }
-
-  //     if (currentScroll > lastScroll && !nav.classList.contains(scrollDown)) {
-  //       nav.classList.remove(scrollUp);
-  //       nav.classList.add(scrollDown);
-  //     } else if (
-  //       currentScroll < lastScroll &&
-  //       nav.classList.contains(scrollDown)
-  //     ) {
-  //       nav.classList.remove(scrollDown);
-  //       nav.classList.add(scrollUp);
-  //     }
-  //     lastScroll = currentScroll;
-  //   });
-  // });
-
   const menuList = menu
     .filter((categoryItem) => category === categoryItem.categoryFrench)
     .map((menuItem) => {
