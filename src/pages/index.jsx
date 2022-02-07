3735e51ef9e18e3eae913a4940abec3370f6768f
Rianne Streef <56265374+RianneStreef@users.noreply.github.com>
2022-02-07 13:52:04 +0100

    back to top link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 import { Helmet } from "react-helmet";
 
 import "../styles/global.css";
 
 import { content } from "../content/languages";
 import intakeInfo from "../content/intake";
+
+import top from "../images/top.png";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -29,6 +32,9 @@ const IndexPage = function (props) {
         <meta name="keywords" content={languageToUse.metaKeywords} />
         <link rel="canonical" href={intakeInfo.domainName} />
       </Helmet>
+      <Link to="/">
+        <img src={top} className="top-link" alt="Back to top" />
+      </Link>
       <Header
         language={language}
         setLanguage={setLanguage}
